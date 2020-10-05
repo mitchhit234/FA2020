0a7db3be29e57c9d5381a71a7233eee0225b3be9
Mitchell Meier <mdmfvz@mst.edu>
2020-10-04 22:45:40 -0500

    lazy git commit

---
+++ assignments/databases/hw2/q3.sql
@@ -0,0 +1,14 @@
+INSERT INTO ORIGIN (ORIGIN_ID, LOCATION)
+VALUES (1600, 'Netherlands'),
+(1601, 'Viena'),
+(1602, 'Madrid');
+
+
+INSERT INTO ART (NAME, ARTIST, ORIGIN_ID, DATING, MEDIA)
+VALUES ('The Walter Lilies', 'Claude Monet', 3543, '1920 to 1928', 'Painting'),
+('Mona Lisa', 'Da Vinci', 3543, '1503 to 1519','Painting'),
+('Girl with a Pearl Earring', 'Johannes Vermeer', 1600, '1665', 'Painting'),
+('The Starry Night', 'Vincent van Gogh', 3543, '1889', 'Painting'),
+('The Kiss', 'Gustav Klimit', 1601, '1907-1908', 'Painting'),
+('The Garden of Earthly Delights', 'Hieronymus Bosch', 1602, '1503-1515', 'Painting')
+
