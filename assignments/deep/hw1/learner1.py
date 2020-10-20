ed4f87cf65163d2d8dd4fef279df8cd3898a1a10
Mitchell Meier <mdmfvz@mst.edu>
2020-10-19 19:02:44 -0500

    lazy git commit

--- assignments/deep/hw1/learner1.py
+++ assignments/deep/hw1/learner1.py
@@ -1,4 +1,4 @@
-import re
+import secrets
 
 class Data:
 	def __init__(self, ck, ch):
@@ -18,11 +18,30 @@ def parse_input(inp):
 	for i in temp:
 		cks = i.split('\t')[0]
 		chs = i.split('\t')[1].replace('\n', '')
-		ret.append(Data(cks, chs))
+		ret.append(Data(int(cks), int(chs)))
 	return ret
+
+def y_hat(weight0, weight1, inp):
+	return (weight0 * inp) + (weight1 * inp)
 
 
 training = parse_input('chocodata.txt')
 validation = parse_input('chocovalid.txt')
 
 
+#Main
+eta = 0.000001
+
+w0 = secrets.randbelow(50) + 1
+w1 = secrets.randbelow(50) + 1
+
+
+for i in range(5000):
+	for k in training:
+		cap = y_hat(w0, w1, k.cookies)
+		delta = k.chips - cap
+		w0 = w0 + eta * delta * k.chips
+		w1 = w1 + eta * delta * k.chips
+
+
+print(abs(w0+w1))
